a73a581eb79cea2b38667970c3522974064b9ea0
Vasya Kortik <37380184+az4z3l@users.noreply.github.com>
2021-05-19 19:22:12 +0300

    Add files via upload

---
+++ tempfunction.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+template <typename T>
+void print(T* massive, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        cout << massive[i] << " ";
+
+    cout << endl;
+}
+
+template <typename T> 
+void msort(T* massive, int size)
+{
+    int i, beg = 0, end = size - 1, m = 0, n = 0;
+    T omin, pmin;
+
+    while ((end - beg) > 0)
+    {
+        omin = pmin = 0;
+
+        for (i = beg; i <= end; i++)
+        {
+            if (massive[i] <= omin)
+            {
+                omin = massive[i];
+                m = i;
+            }
+
+            if (massive[i] >= 0 && (massive[i] <= pmin || pmin == 0))
+            {
+                pmin = massive[i];
+                n = i;
+            }
+        }
+
+        if ((end - beg) != 1)
+        {
+            massive[m] = massive[beg];
+            massive[beg] = omin;
+
+            massive[n] = massive[end];
+            massive[end] = pmin;
+        }
+        
+        else // если размер чётный
+        {
+            if (massive[beg] > massive[end])
+            {
+                pmin = massive[end];
+                massive[end] = massive[beg];
+                massive[beg] = pmin;
+            }
+        }
+        
+        beg++;
+        end--;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double a[5] = { 243.321, -452, 34, 0, -2 };
+    char b[6] = { 54, 57, 54, -43, -60, -71};
+
+    cout << "Исходные массивы:" << endl;
+    print(a, 5);
+    print(b, 6);
+
+    cout << "Идёт обработка..." << endl;
+    msort(a, 5);
+    msort(b, 6);
+
+    cout << "Нажмите для подтверждения...";
+    getchar();
+
+    cout << "Обработанные массивы:" << endl;
+    print(a, 5);
+    print(b, 6);
+
+    cout << "Нажмите для подтверждения...";
+    getchar();
+
+    return 0;
+}
+
+
---
+++ tempkind.cpp
@@ -0,0 +1,157 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+template <class Data> class Stack
+{
+private:
+
+    Data* array;
+    int top;
+    int size;
+
+public:
+
+    Stack()
+    {
+        array = NULL;
+        top = -1;
+        size = 0;
+    }
+
+    Stack(int n)
+    {
+        size = n;
+        top = -1;
+        array = new Data[size];
+    }
+
+    ~Stack()
+    {
+        delete[] array;
+    }
+
+    bool Full()
+    {
+        return top == (size - 1);
+    }
+
+    bool Empty()
+    {
+        return top == -1;
+    }
+
+    Data Top()
+    {
+        if(!Empty())
+            return array[top];
+    }
+
+    Data Pop()
+    {
+        if(!Empty())
+            return array[top--];
+    }
+
+    bool Push(Data in)
+    {
+        if (Full())
+            return false;
+            
+        array[++top] = in;
+        return true;
+    }
+
+};
+
+template <class T>
+void interface(T & stack)
+{
+    char choice;
+    int in;
+
+    do
+    {
+        system("cls");
+
+        cout << "[P]op | P[u]sh | [T]op | [Н]азад" << endl;
+
+        cin >> choice;
+        getchar();
+
+        switch (choice)
+        {
+        case 'P': case 'p':
+
+            if (stack.Empty())
+                cout << "Стек пуст." << endl;
+            else
+                cout << stack.Pop() << endl;
+
+            break;
+
+        case 'U': case 'u':
+
+            cout << "Введите значение: ";
+            cin >> in;
+            getchar();
+
+            if (!stack.Push(in))
+            {
+                cout << "Стек заполнен." << endl;
+            }
+
+            break;
+
+        case 'T': case 't':
+
+            if (stack.Empty())
+                cout << "Стек пуст." << endl;
+            else
+                cout << stack.Top() << endl;
+
+            break;
+        }
+
+        cout << "Нажмите для подтверждения..." << endl;
+        getchar();
+
+    } while (choice != 'Н');
+
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    char choice;
+    Stack <double> dstack(5);
+    Stack <char> cstack(5);
+
+    do
+    {
+        system("cls");
+
+        cout << "Стек [D]ouble | Стек [C]har | [В]ыход" << endl;
+
+        cin >> choice;
+        getchar();
+
+        switch (choice)
+        {
+        case 'D': case 'd':
+
+            interface(dstack);
+            break;
+
+        case 'C': case 'c':
+
+            interface(cstack);
+            break;
+        }
+    } while (choice != 'В');
+
+}
